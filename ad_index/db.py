1bae0c108853961df699c32e0a2d1380b295db42
Alex Nichol <unixpickle@gmail.com>
2023-08-05 16:36:46 -0400

    simplify DB code with @transaction decorator

--- ad_index/db.py
+++ ad_index/db.py
@@ -5,7 +5,17 @@ import sqlite3
 from contextlib import asynccontextmanager
 from dataclasses import dataclass
 from functools import partial
-from typing import Awaitable, Callable, Iterator, List, Optional, Tuple, TypeVar, Union
+from typing import (
+    Any,
+    Awaitable,
+    Callable,
+    Iterator,
+    List,
+    Optional,
+    Tuple,
+    TypeVar,
+    Union,
+)
 
 import aiosqlite
 from httpx import Client
@@ -35,6 +45,21 @@ class PushQueueItem:
     retries: int
 
 
+def transaction(fn: Callable[..., Awaitable[Any]]) -> Callable[..., Awaitable[Any]]:
+    async def new_fn(db: "DB", *args, **kwargs) -> Any:
+        async with db._lock:
+            await db._retry(partial(db._conn.execute, "BEGIN TRANSACTION"))
+            try:
+                result = await db._retry(partial(fn, db, *args, **kwargs))
+            except:
+                await db._retry(partial(db._conn.execute, "ROLLBACK"))
+                raise
+            await db._retry(partial(db._conn.execute, "COMMIT"))
+            return result
+
+    return new_fn
+
+
 class DB:
     def __init__(self, conn: aiosqlite.Connection):
         self._conn = conn
@@ -48,6 +73,7 @@ class DB:
             await db._create_tables()
             yield db
 
+    @transaction
     async def _create_tables(self):
         await self._conn.execute(
             """
@@ -101,133 +127,110 @@ class DB:
             CREATE INDEX IF NOT EXISTS push_queue_retry_time ON push_queue(retry_time)
             """
         )
-        await self._conn.commit()
 
+    @transaction
     async def ad_queries(self) -> Iterator[AdQuery]:
-        async with self._lock:
-            async with await self._retry(
-                partial(
-                    self._conn.execute,
-                    "SELECT ad_query_id, nickname, query, filters FROM ad_queries",
+        async with await self._conn.execute(
+            "SELECT ad_query_id, nickname, query, filters FROM ad_queries"
+        ) as results:
+            async for row in results:
+                yield AdQuery(
+                    ad_query_id=row[0],
+                    nickname=row[1],
+                    query=row[2],
+                    filters=json.loads(row[3]),
                 )
-            ) as results:
-                async for row in results:
-                    yield AdQuery(
-                        ad_query_id=row[0],
-                        nickname=row[1],
-                        query=row[2],
-                        filters=json.loads(row[3]),
-                    )
 
+    @transaction
     async def insert_ad_query(self, q: AdQuery) -> int:
-        async with self._lock:
-            result = await self._retry(
-                partial(
-                    self._conn.execute_insert,
-                    "INSERT INTO ad_queries (nickname, query, filters) VALUES (?, ?, ?)",
-                    (q.nickname, q.query, json.dumps(q.filters)),
-                )
-            )
-            await self._retry(self._conn.commit)
-            return result
+        return await self._conn.execute_insert(
+            "INSERT INTO ad_queries (nickname, query, filters) VALUES (?, ?, ?)",
+            (q.nickname, q.query, json.dumps(q.filters)),
+        )
 
-    async def update_ad_query(self, q: AdQuery):
-        async with self._lock:
-            await self._retry_execute(
+    @transaction
+    async def update_ad_query(self, q: AdQuery) -> bool:
+        return (
+            await self._conn.execute(
                 "REPLACE INTO ad_queries (ad_query_id, nickname, query, filters) VALUES (?, ?, ?)",
                 (q.ad_query_id, q.nickname, q.query, json.dumps(q.filters)),
             )
+        ).rowcount != 0
 
-    async def delete_ad_query(self, id: int):
-        async with self._lock:
-            await self._retry_execute_commit(
-                "DELETE FROM ad_queries WHERE ad_query_id=?", id
+    @transaction
+    async def delete_ad_query(self, id: int) -> bool:
+        return (
+            await self._conn.execute(
+                "DELETE FROM ad_queries WHERE ad_query_id=?", (id,)
             )
+        ).rowcount != 0
 
+    @transaction
     async def create_session(
         self, vapid_pub: bytes, vapid_priv: bytes, session_id: str
     ):
-        async with self._lock:
-            hash = hash_session_id(session_id)
-            await self._retry_execute_commit(
-                "INSERT INTO clients (vapid_pub, vapid_priv, session_id, session_hash) VALUES (?, ?, ?, ?)",
-                (vapid_pub, vapid_priv, session_id, hash),
-            )
+        hash = hash_session_id(session_id)
+        await self._conn.execute(
+            "INSERT INTO clients (vapid_pub, vapid_priv, session_id, session_hash) VALUES (?, ?, ?, ?)",
+            (vapid_pub, vapid_priv, session_id, hash),
+        )
 
+    @transaction
     async def update_client_push_sub(
         self, session_id: str, push_sub: Optional[str]
     ) -> bool:
-        async with self._lock:
-            hash = hash_session_id(session_id)
-            count = await self._retry_execute_commit(
-                "UPDATE clients SET push_sub=? WHERE session_hash=?",
-                (push_sub, hash),
-            )
-            return count != 0
-
-    async def get_client_push_info(self, session_id: str) -> Optional[ClientPushInfo]:
-        async with self._lock:
-            hash = hash_session_id(session_id)
-            cursor = await self._retry(
-                partial(
-                    self._conn.execute,
-                    "SELECT push_sub, vapid_priv FROM clients WHERE session_hash=?",
-                    (hash,),
-                )
-            )
-            async for row in cursor:
-                return ClientPushInfo(push_sub=row[0], vapid_priv=row[1])
-            return None
-
-    async def push_queue_next(self, retry_timeout: int) -> Optional[PushQueueItem]:
-        async with self._lock:
-            async with await self._retry(self._conn.cursor) as tx:
-                cursor = await tx.execute(
-                    """
-                    SELECT
-                        push_queue.id,
-                        push_queue.message,
-                        push_queue.retries,
-                        clients.push_sub,
-                        clients.vapid_priv
-                    FROM push_queue
-                    LEFT JOIN clients ON clients.client_id = push_queue.client_id
-                    WHERE retry_time <= STRFTIME('%s')
-                    ORDER BY retry_time
-                    """
-                )
-                row = None
-                async for row in cursor:
-                    break
-                if row is None:
-                    return None
-                id, message, retries, push_sub, vapid_priv = row
-                await tx.execute(
-                    "UPDATE push_queue SET retry_time=STRFTIME('%s')+?, retries=? WHERE id=?",
-                    (retry_timeout, retries + 1, id),
-                )
-                await tx.execute("commit")
-                return PushQueueItem(
-                    id=id,
-                    push_info=ClientPushInfo(push_sub=push_sub, vapid_priv=vapid_priv),
-                    message=message,
-                    retries=retries,
-                )
-
-    async def push_queue_finish(self, id: int):
-        async with self._lock:
-            await self._retry_execute_commit("DELETE FROM push_queue WHERE id=?", (id,))
-
-    async def _retry_execute_commit(self, *args) -> int:
-        cursor = await self._retry(
-            partial(
-                self._conn.execute,
-                *args,
-            )
+        hash = hash_session_id(session_id)
+        count = await self._conn.execute(
+            "UPDATE clients SET push_sub=? WHERE session_hash=?",
+            (push_sub, hash),
         )
-        count = cursor.rowcount
-        await self._retry(self._conn.commit)
-        return count
+        if count != 0 and not push_sub:
+            await self._conn.execute(
+                """
+                DELETE FROM push_queue WHERE client_id=(
+                    SELECT client_id FROM clients WHERE session_hash=?
+                )
+                """,
+                hash,
+            )
+        return count != 0
+
+    @transaction
+    async def push_queue_next(self, retry_timeout: int) -> Optional[PushQueueItem]:
+        cursor = await self._conn.execute(
+            """
+            SELECT
+                push_queue.id,
+                push_queue.message,
+                push_queue.retries,
+                clients.push_sub,
+                clients.vapid_priv
+            FROM push_queue
+            LEFT JOIN clients ON clients.client_id = push_queue.client_id
+            WHERE retry_time <= STRFTIME('%s')
+            ORDER BY retry_time
+            """,
+        )
+        row = None
+        async for row in cursor:
+            break
+        if row is None:
+            return None
+        id, message, retries, push_sub, vapid_priv = row
+        await self._conn.execute(
+            "UPDATE push_queue SET retry_time=STRFTIME('%s')+?, retries=? WHERE id=?",
+            (retry_timeout, retries + 1, id),
+        )
+        return PushQueueItem(
+            id=id,
+            push_info=ClientPushInfo(push_sub=push_sub, vapid_priv=vapid_priv),
+            message=message,
+            retries=retries,
+        )
+
+    @transaction
+    async def push_queue_finish(self, id: int):
+        await self._conn.execute("DELETE FROM push_queue WHERE id=?", (id,))
 
     async def _retry(self, fn: Callable[[], Awaitable[R]]) -> R:
         while True:
